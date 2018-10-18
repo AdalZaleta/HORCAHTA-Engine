4705f64a8bb0901e736e7263b5a4540453c9a0e2
Your NameLB <luisbazan417@gmail.com>
2018-10-18 16:27:05 -0500

    Proyecto base

---
+++ Horchata_Engine/RenderGL.cpp
@@ -0,0 +1,68 @@
+#include "RenderGL.h"
+#include "SDL_opengl.h"
+#include <GL\GLU.h>
+
+RenderGL g_renderGL; //Singleton
+
+RenderGL::RenderGL()
+{
+}
+RenderGL::~RenderGL()
+{
+
+}
+
+void RenderGL::inicializar()
+{
+	GLenum error = GL_NO_ERROR;
+	//Para obtener error: error = glGetError();
+
+	//Inicializamos Matrix
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	//Model view Matrix
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	
+	float aspect =(float)w / (float)h;
+
+	if (w >= h)
+	{
+		clipAreaXLeft=-10.0*aspect;
+		clipAreaXRightt = 10.0*aspect;
+		clipAreaYBottom = -10.0;
+		clipAreaYTop = 10.0;
+	}
+	else
+	{
+		clipAreaXLeft = -10.0;
+		clipAreaXRightt = 10.0;
+		clipAreaYBottom = -10.0/aspect;
+		clipAreaYTop = 10.0/aspect;
+	}
+
+	gluOrtho2D(clipAreaXLeft, clipAreaXRightt, clipAreaYBottom, clipAreaYTop);
+	//Initialize clear color
+	glClearColor(0.f, 0.f, 0.f, 1.f);
+
+}
+
+void RenderGL::liberar()
+{
+	//delete this
+}
+
+void RenderGL::update()
+{
+	
+}
+
+void RenderGL::render()
+{
+	//Limpiamos pantalla
+	glClear(GL_COLOR_BUFFER_BIT);
+
+}
+
+
---
+++ Horchata_Engine/RenderGL.h
@@ -0,0 +1,28 @@
+#pragma once
+
+class RenderGL
+{
+	public:
+		RenderGL();
+		~RenderGL();
+
+		float clipAreaXLeft;
+		float clipAreaYTop;
+		float clipAreaXRightt;
+		float clipAreaYBottom;
+		int h;
+		int w;
+		//Funcion encarga de inicializar OpenGL
+		//Regresa True si se inicio correctamente o false en caso cotrario
+		void inicializar();
+		//Se llama al momento de libarse
+		void liberar();
+
+		//Funcion que se actualiza cada frame
+		void update();
+		//Funcion encargado de dibujar
+		void render();
+};
+
+extern RenderGL g_renderGL; //Singleton
+
