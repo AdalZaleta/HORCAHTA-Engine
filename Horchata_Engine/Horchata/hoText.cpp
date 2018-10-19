d650dad8a722d50416ec7a982573b0c12c963459
Galaxyben <galaxyben1@gmail.com>
2018-10-19 15:13:15 -0500

    Pepe part

--- Horchata_Engine/Horchata/hoText.cpp
+++ Horchata_Engine/Horchata/hoText.cpp
@@ -1,1 +1,24 @@
 #include "hoText.h"
+
+//EJEMPLO LIBRO 3
+/*void hoText::glTextOut(long start_x, long start_y, char *string)
+{
+	char file[50];
+	long h = 12;
+	long w = 8;
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	for (long idx = 0; idx < (signed)strlen(string); idx++)
+	{
+		if (string[idx] != ' ')
+		{
+			sprintf(file, "font1_%c.bmp", tolower(string[idx]));
+			glBindTexture(GL_TEXTURE_2D, TextureName[GetTextureID(file)]);
+			glBitBlt(GetTextureID(file), start_x + (idx*w), start_y, w, h);
+		}
+	}
+	glDisable(GL_BLEND);
+}
+*/
+
+//Ejemplo libro 4
--- Horchata_Engine/Horchata/hoText.h
+++ Horchata_Engine/Horchata/hoText.h
@@ -1,10 +1,25 @@
 #pragma once
 
 #include "SDL.h"
+#include "SDL_opengl.h"
+#include <GL\GLU.h>
+#include <stdio.h>
+#include "SDL_ttf.h"
 
 class hoText {
 public:
+	void glTextOut(long start_x, long start_y, char *string);
 	
+	int TTF_Init(void);
+	void TTF_Quit(void);
+	void atexit();
+	TTF_Font *TTF_OpenFont(const char *file, int ptsize);
+	TTF_Font *TTF_OpenFontIndex(const char *file, int ptsize, long index);
+	void TTF_Close(TTF_Font *font);
+	int TTF_FontHeight(TTF_Font * font);
+	int TTF_FontAscent(TTF_Font * font);
+	int TTF_FontDescent(TTF_Font * font);
+	//Pagina 347
 
 private:
 
