d49158fb47377f56ebf4080d45ef7178b3063993
Galaxyben <galaxyben1@gmail.com>
2018-10-25 16:24:06 -0500

    commit

--- Horchata_Engine/Horchata/hoText.h
+++ Horchata_Engine/Horchata/hoText.h
@@ -10,16 +10,35 @@ class hoText {
 public:
 	void glTextOut(long start_x, long start_y, char *string);
 	
-	int TTF_Init(void);
-	void TTF_Quit(void);
-	void atexit();
+
 	TTF_Font *TTF_OpenFont(const char *file, int ptsize);
 	TTF_Font *TTF_OpenFontIndex(const char *file, int ptsize, long index);
 	void TTF_Close(TTF_Font *font);
+
+	//FUNCIONES PARA OBTENER INFORMACIÓN DE LA FUENTE
 	int TTF_FontHeight(TTF_Font * font);
 	int TTF_FontAscent(TTF_Font * font);
 	int TTF_FontDescent(TTF_Font * font);
-	//Pagina 347
+
+	//FUNCIONES PARA MANEJAR FUENTES
+	int TTF_GetFontStyle(TTF_Font * font);
+	void TTF_SetFontStyle(TTF_Font *font, int style);
+	int TTF_FontLineSkip(TTF_Font *font);
+	int TTF_SizeText(TTF_Font *font, const char *text, int *w, int *h);
+	int TTF_SizeUTF8(TTF_Font *font, const char *text, int *w, int *h);
+	int TTF_SizeUNICODE(TTF_Font *font, const Uint16 * text, int * w, int * h);
+
+	//FUNCIONES PARA RENDERIZADO DE TEXTO
+	SDL_Surface * TTF_RenderText_Solid(TTF_Font *font, const char *text, SDL_Color fg);
+	SDL_Surface * TTF_RenderText_Shaded(TTF_Font *font, const char *text, SDL_Color fg, SDL_Color bg);
+	SDL_Surface * TTF_RenderText_Blended(TTF_Font *font, const char *text, SDL_Color fg);
+
+	SDL_Surface * TTF_RenderUTF8_Solid(TTF_Font *font, const char *text, SDL_Color fg);
+	SDL_Surface * TTF_RenderUNICODE_Solid(TTF_Font *font, const Uint16 *text, SDL_Color fg);
+	SDL_Surface * TTF_RenderUTF8_Shaded(TTF_Font *font, const char *text, SDL_Color fg, SDL_Color bg);
+	SDL_Surface * TTF_RenderUNICODE_Shaded(TTF_Font *font, const Uint16 *text, SDL_Color fg, SDL_Color bg);
+	SDL_Surface * TTF_RenderUTF8_Blended(TTF_Font *font, const char *text, SDL_Color fg, SDL_Color bg);
+	SDL_Surface * TTF_RenderUNICODE_Blended(TTF_Font *font, const Uint16 *text, SDL_Color fg, SDL_Color bg);
 
 private:
 
