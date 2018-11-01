ee3b5cd5b56398f3ce1a50e69cf454d107c8fa68
AdalZaleta <32682010+AdalZaleta@users.noreply.github.com>
2018-11-01 15:48:23 -0600

    Comments part of code

--- Horchata_Engine/Horchata/hoImages.h
+++ Horchata_Engine/Horchata/hoImages.h
@@ -9,13 +9,17 @@ class hoImages
 {
 public:
 	hoImages();
+	//Contructor of the class with the direction of the Image
 	hoImages(const char* _dirImages);
 	~hoImages();
 
+	//This function Draw a pre-load Image
 	void DrawImage(int _x, int _y, int _width, int _height);
+	//This function Load a Image in the path
+	//The path star in the folder 'Resources'
 	bool LoadImage_(const char *_dirImages);
 	GLuint textureID;
 private:
-	const char* dirImage;
+	//This is the surface of the image will be loaded
 	SDL_Surface* imageSurface = NULL;
 };
