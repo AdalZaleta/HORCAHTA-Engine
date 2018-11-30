83aaf43477b680403fa75bd2c2b7070686727926
DiegoADB <diegoadb17@gmail.com>
2018-11-30 14:26:45 -0600

    Listos comentarios

--- Horchata_Engine/Horchata/hoSprite.cpp
+++ Horchata_Engine/Horchata/hoSprite.cpp
@@ -9,44 +9,44 @@ hoSprite::~hoSprite()
 {
 }
 
-void hoSprite::DrawSprite(int _x, int _y)
+void hoSprite::Draw(int _x, int _y)
 {
 	glPushMatrix(); //Se hace un PushMatrix() para poder trasladarnos en las 4 esquinas de la imagen
-		glColor4f(color[0], color[1], color[2], color[3]);
-		glRotatef(angle, 0, 0, 1);
-		DrawImage(_x, _y, width / 100.0f * scalex, height / 100.0f * scaley);
+		glColor4f(color[0], color[1], color[2], color[3]); //Dibujamos el color (blanco por default)
+		glRotatef(angle, 0, 0, 1); //rotamos en Z
+		DrawImage(_x, _y, width / 100.0f * scalex, height / 100.0f * scaley); //Se dibuja como imagen
 	glPopMatrix(); //Termina la fase de render
 }
 
-void hoSprite::DrawSprite(int _x, int _y, float rotation)
+void hoSprite::Draw(int _x, int _y, float rotation) //Sobrecarga que settea la rotacion
 {
 	angle = rotation;
-	DrawSprite(_x, _y);
+	Draw(_x, _y);
 }
 
-void hoSprite::SetScaleSprite(float scaleX, float scaleY)
+void hoSprite::SetScale(float scaleX, float scaleY) //Settea la escala
 {
 	scalex = scaleX;
 	scaley = scaleY;
 }
 
-void hoSprite::SetScaleSprite(float scale)
+void hoSprite::SetScale(float scale) //Sobrecarga del metodo de setteo de escala
 {
 	scalex = scale;
 	scaley = scale;
 }
 
-void hoSprite::SetRotationSprite(float _angle)
+void hoSprite::SetRotation(float _angle) //Settea la rotacion
 {
 	angle = _angle;
 }
 
-void hoSprite::SetColorSprite(float _color[4])
+void hoSprite::SetColor(float _color[4]) //Settea el color (recibe un float[4] {r, g, b, a})
 {
 	for (int i = 0; i < 4; i++) color[i] = _color[i];
 }
 
-void hoSprite::SetAlphaSprite(float alpha)
+void hoSprite::SetAlpha(float alpha) //Settea solo el alpha, manteniendo el color
 {
 	color[3] = alpha;
 }
--- Horchata_Engine/Horchata/hoSprite.h
+++ Horchata_Engine/Horchata/hoSprite.h
@@ -12,12 +12,12 @@ public:
 	float angle = 0;
 	float color[4] = { 1, 1, 1, 1 }; //El color del sprite inicia en blanco, 1 de alpha
 
-	void DrawSprite(int x, int y);
-	void DrawSprite(int x, int y, float rotation);
-	void SetScaleSprite(float scaleX, float scaleY);
-	void SetScaleSprite(float scale);
-	void SetRotationSprite(float angle);
-	void SetColorSprite(float color[4]);
-	void SetAlphaSprite(float alpha);
+	void Draw(int x, int y);
+	void Draw(int x, int y, float rotation);
+	void SetScale(float scaleX, float scaleY);
+	void SetScale(float scale);
+	void SetRotation(float angle);
+	void SetColor(float color[4]);
+	void SetAlpha(float alpha);
 };
 
