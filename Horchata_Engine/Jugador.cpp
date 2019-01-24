8f41d9469f5deab95b10d9ae92c451f3bfc5dd72
TerrierBrakk <32682025+TerrierBrakk@users.noreply.github.com>
2019-01-24 16:58:11 -0600

    Solo marcando atributos para jugador

---
+++ Horchata_Engine/Jugador.cpp
@@ -0,0 +1,34 @@
+#include "Jugador.h"
+
+
+
+Jugador::Jugador()
+{
+}
+
+
+void Jugador::cambiarTurno()
+{
+	!turno;
+
+}
+
+Jugador::~Jugador()
+{
+}
+/*
+Inicia un Jugador ( X )
+Jugador tiene Turnos (turno 1-2)
+Jugador Pone Marca
+Jugador tiene distinta marca
+La marca es un sprite
+La marca no se modifica 
+La marca se pone en un tablero
+tablero tiene spots(9)
+el spot esta vacio
+Se gana si un jugador hace tres marcas en linea o diagonal//se empata si no se cumple lo anterior.
+Contador de V/E/D
+Se reinicia
+|
+
+*/
---
+++ Horchata_Engine/Jugador.h
@@ -0,0 +1,15 @@
+#pragma once
+class Jugador
+{
+public:
+	Jugador();
+	
+
+	bool turno;
+
+	void cambiarTurno();
+
+
+	virtual ~Jugador();
+};
+
